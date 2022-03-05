307728633ce41b6d9f32470c68f19967040c6968
bgundla3 <bhargavigundla@gatech.edu>
2022-03-05 16:55:45 -0500

    added sound macros

--- HW3Lib.h
+++ HW3Lib.h
@@ -27,6 +27,51 @@ typedef unsigned short u16;
 #define REG_SOUNDCNT_L *(volatile u16*)0x04000080 
 #define REG_SOUNDCNT_H *(volatile u16*)0x04000082 // bits 0-1 are DMG sound volume. 00 => 25%, 01 => 50%, 10 => 100%
 
+
+// Other Sound Macros
+#define REG_SND1SWEEP *(volatile u16*)0x04000060
+#define REG_SND1CNT *(volatile u16*)0x04000062
+#define REG_SND1FREQ *(volatile u16*)0x04000064
+#define REG_SND2CNT *(volatile u16*)0x04000068
+#define REG_SND2FREQ *(volatile u16*)0x0400006C
+#define REG_SND3SEL *(volatile u16*)0x04000070
+#define REG_SND3CNT *(volatile u16*)0x04000072
+#define REG_SND3FREQ *(volatile u16*)0x04000074
+#define REG_SND4CNT *(volatile u16*)0x04000078
+#define REG_SND4FREQ *(volatile u16*)0x0400007C
+
+// Channel 3 Wave Pattern RAM (2 banks!!)
+#define REG_SND3_WAV *(volatile u16*)0x04000090
+#define DMG_SND1_LEFT (1 << 8)
+#define DMG_SND2_LEFT (1 << 9)
+#define DMG_SND3_LEFT (1 << 10)
+#define DMG_SND4_LEFT (1 << 11)
+#define DMG_SND1_RIGHT (1 << 12)
+#define DMG_SND2_RIGHT (1 << 13)
+
+#define DMG_SND3_RIGHT (1 << 14)
+#define DMG_SND4_RIGHT (1 << 15)
+
+// n: [0-7]
+#define DMG_VOL_LEFT(n) (((n) & 7) << 0)
+#define DMG_VOL_RIGHT(n) (((n) & 7) << 4)
+
+// n: [0-15]
+#define DMG_ENV_VOL(n) (((n) & 15) << 12)
+
+// n: [0-7]
+#define DMG_STEP_TIME(n) (((n) & 7) << 8)
+#define DMG_DIRECTION_DECR (0 << 11)
+#define DMG_DIRECTION_INCR (1 << 11)
+#define DMG_DUTY_12 (0 << 6)
+#define DMG_DUTY_25 (1 << 6)
+#define DMG_DUTY_50 (2 << 6)
+#define DMG_DUTY_75 (3 << 6)
+
+// n: [0-2]
+#define DMG_MASTER_VOL(n) ((n) % 3)
+#define SND_RESET (1<<15)
+
 // Video Buffer
 extern volatile unsigned short *videoBuffer;
 
